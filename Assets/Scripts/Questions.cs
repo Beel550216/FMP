f753b7b3391be4cea6dce5074e8646d0beb69ec0
Beel550216 <550216@chrome.escg.ac.uk>
2025-03-06 11:49:01 +0000

    Initial commit

---
+++ Assets/Scripts/Questions.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+public class Test : MonoBehaviour
+{
+
+    private List<QuestionData> m_Questions;
+
+    void Start()
+    {
+        m_Questions = new List<QuestionData>();
+        ShowQuestion(GetQuestion());
+
+    }
+
+    private void ShowQuestion(QuestionData lQuestion)
+    {
+        // Show your question etc.
+    }
+
+    public void AnswerQuestionButton(bool correct, float value)
+    {
+        if (correct)
+        {
+            // do your stuff here for a good answer.
+        }
+        else
+        {
+            // the player is not so smart this time. 
+        }
+    }
+
+
+    private QuestionData GetQuestion()
+    {
+        QuestionData lQuestion = m_Questions[Random.Range(0, m_Questions.Count)];
+        m_Questions.Remove(lQuestion);
+        return lQuestion;
+    }
+
+
+    public class QuestionData
+    {
+        public string question;
+        public string answer;
+    }
+
+}
+*/
