495bb68d2963be7051110a207ec0f78825d2be00
Beel550216 <550216@chrome.escg.ac.uk>
2025-03-10 07:55:50 +0000

    1

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public static LevelManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            print("do not destroy");
+        }
+        else
+        {
+            print("do destroy");
+            Destroy(gameObject);
+        }
+
+    }
+}
