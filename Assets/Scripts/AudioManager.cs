98f5f7e3fa58b2708f4cbf687bef43833a46c8f7
Beel550216 <550216@chrome.escg.ac.uk>
2025-04-06 17:53:52 +0100

    26

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,15 +1,20 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
 
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance;
+    [SerializeField] AudioMixer mixer;
 
     [SerializeField] AudioSource audienceSource;
     [SerializeField] List<AudioClip> audienceClips = new List<AudioClip>();
+    
 
-    const string Music_Key = "musicVolume";
-    const string SFX_Key = "sfxVolume";
+    public const string Music_Key = "musicVolume";
+    public const string SFX_Key = "sfxVolume";
 
     void Awake()
     {
@@ -25,11 +30,23 @@ public class AudioManager : MonoBehaviour
             Destroy(gameObject);
         }
 
+        LoadVolume();
+
     }
 
     public void AudienceSFX()
     {
         AudioClip clip = audienceClips[0];
         audienceSource.PlayOneShot(clip);
+    }
+
+
+    void LoadVolume()
+    {
+        float musicVolume = PlayerPrefs.GetFloat(Music_Key, 1f);
+        float sfxVolume = PlayerPrefs.GetFloat(SFX_Key, 1f);
+
+        mixer.SetFloat(VolumeSettings.Mixer_Music, Mathf.Log10(musicVolume)* 20);
+        mixer.SetFloat(VolumeSettings.Mixer_SFX, Mathf.Log10(sfxVolume)* 20);
     }
 }
